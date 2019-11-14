a22c974c9201601d9ffad0fd0d921e4d449fd05d
U-UNIVERSITY\100474972 <100474972@00D8614727DB.university.ds.derby.ac.uk>
2019-11-14 15:43:11 +0000

    Final Submission

---
+++ UNiDAYS Discounts Coding Challenge/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UNiDAYS_Discounts_Coding_Challenge
+{
+    class Program
+    {
+        /// <summary>
+        /// Performs discounts on Items A, B, C, D and E
+        /// </summary>
+        /// <param name="item_list"></param>
+        /// <returns></returns>
+        static double Pricing_Rules(Dictionary<char, int> item_list)
+        {
+            double total_price_discount = 0;
+            foreach (var item in item_list)
+            {
+                if (item.Key == 'A')
+                {
+                    total_price_discount += item.Value * 8;
+                }
+                else if (item.Key == 'B')
+                {
+                    if (item.Value % 2 == 0)
+                    {
+                        total_price_discount += ((item.Value / 2) * 20);
+                    }
+                    else
+                    {
+                        total_price_discount += (((item.Value - 1) / 2) * 20) + 12;
+                    }
+                }
+                else if (item.Key == 'C')
+                {
+                    if (item.Value % 3 == 0)
+                    {
+                        total_price_discount += ((item.Value / 2) * 10);
+                    }
+                    else if (item.Value % 3 == 1)
+                    {
+                        total_price_discount += (((item.Value - 1) / 2) * 10) + 4;
+                    }
+                    else
+                    {
+                        total_price_discount += (((item.Value - 2) / 2) * 10) + 8;
+                    }
+                }
+                else if (item.Key == 'D')
+                {
+                    if (item.Value % 2 == 0)
+                    {
+                        total_price_discount += (item.Value * 7) / 2;
+                    }
+                    else
+                    {
+                        total_price_discount += (((item.Value - 1) * 7) / 2) + 7;
+                    }
+                }
+                else if (item.Key == 'E')
+                {
+                    if (item.Value % 3 == 0)
+                    {
+                        total_price_discount += item.Value * (2.0 / 3.0) * 5;
+                    }
+                    else if (item.Value % 3 == 1)
+                    {
+                        total_price_discount += ((item.Value - 1) * 5 * (2.0 / 3.0)) + 5;
+                    }
+                    else
+                    {
+                        total_price_discount += ((item.Value - 2) * 5 * (2.0 / 3.0)) + 10;
+                    }
+                }
+            }
+
+            return total_price_discount;
+        }
+
+        static void Main(string[] args)
+        {
+            Total_Cost result;
+            double totalPrice;
+            int deliveryCharge;
+            double overallTotal;
+
+            UnidaysDiscountChallenge example = new UnidaysDiscountChallenge(Pricing_Rules);
+            example.AddToBasket(""); //<----User inputs their items here. 
+            example.AddToBasket(""); //<----User inputs their items here. 
+
+            result = example.CalculateTotalPrice();
+            totalPrice = result.Total;
+            deliveryCharge = result.DeliveryCharge;
+            overallTotal = totalPrice + deliveryCharge;
+            Console.WriteLine($"Total Price: £{totalPrice}," +
+            $" Delivery Charge: £{deliveryCharge}," +
+            $" Overall Total: £{overallTotal}");
+        }
+    }
+}
---
+++ UNiDAYS Discounts Coding Challenge/UnidaysDiscountChallenge .cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UNiDAYS_Discounts_Coding_Challenge
+{
+    public class UnidaysDiscountChallenge
+    {
+        protected Func<Dictionary<char, int>, double> _pricingRules;
+        protected Dictionary<char, int> item_list = new Dictionary<char, int>();
+       
+        /// <summary>
+        /// Defines the dictionary to store the items and the pricing rules method.
+        /// </summary>
+        /// <param name="rules"></param>
+        public UnidaysDiscountChallenge(Func<Dictionary<char, int>, double> rules)
+        {
+            item_list.Add('A', 0);
+            item_list.Add('B', 0);
+            item_list.Add('C', 0);
+            item_list.Add('D', 0);
+            item_list.Add('E', 0);
+            _pricingRules = rules;
+        }
+       
+        /// <summary>
+        /// Only adds items A, B, C, D and E to the basket.
+        /// </summary>
+        /// <param name="item"></param>
+        public void AddToBasket(string item)
+        {
+            item = item.ToUpper();
+            foreach (char c in item)
+            {
+                if (item_list.ContainsKey(c))
+                {
+                    item_list[c]++; 
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Sets the values of the Total_Cost struct and returns it.
+        /// </summary>
+        /// <returns></returns>
+        public Total_Cost CalculateTotalPrice()
+        {
+            Total_Cost _cost = new Total_Cost();
+            _cost.Total = Get_Total_Price();
+            _cost.DeliveryCharge = DeliveryCharge(_cost.Total);
+            return _cost;
+        }
+        
+        /// <summary>
+        /// Gets and returns the total discounted cost of the order.
+        /// </summary>
+        /// <returns></returns>
+        public double Get_Total_Price()
+        {
+           return  _pricingRules(item_list);
+        }
+        
+        /// <summary>
+        /// Calculates the delivery charge of the order.
+        /// </summary>
+        /// <param name="total_price"></param>
+        /// <returns></returns>
+        public int DeliveryCharge(double total_price)
+        {
+            if (total_price < 50 && total_price > 0)
+            {
+                return 7;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    /// <summary>
+    /// Holds a double for total discounted price and an int for the delivery charge.
+    /// </summary>
+    public struct Total_Cost
+    {
+        public double Total;
+        public int DeliveryCharge;
+    }
+}
